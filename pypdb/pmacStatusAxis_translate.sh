0547a256fcdddf6e76f823e92b9293c54ba78b55
Wayne Lewis <wlewis@bnl.gov>
2012-04-16 11:59:27 -0400

    [#227] Create NSLS-II version of pmacStatusAxis.db and add translation script and translations file.

---
+++ pypdb/pmacStatusAxis_translate.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+IOC_PATH=~/local/data/work/hg/repos/modules/pmacUtil
+applypvs  -i $IOC_PATH/pypdb/pmacUtil_pmacStatusAxis.pot \
+	-o $IOC_PATH/fls \
+	$IOC_PATH/pmacUtilApp/Db/pmacStatusAxis.db
