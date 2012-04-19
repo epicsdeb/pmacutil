2bfe8d9c2af19570c3a4e9c30c4f99ec65cfdffc
Wayne Lewis <wlewis@bnl.gov>
2012-04-18 21:52:48 -0400

    [#227] Fix up translation script.

--- pypdb/pmac_asyn_motor_translate_db.sh
+++ pypdb/pmac_asyn_motor_translate_db.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 IOC_PATH=~/local/data/work/hg/repos/modules/pmacUtil
-applypvs  -i $IOC_PATH/pypdb/pmacUtil_pmacStatus.pot \
+applypvs  -i $IOC_PATH/pypdb/pmacUtil_pmac_asyn_motor.pot \
 	-o $IOC_PATH/fls \
-	$IOC_PATH/pmacUtilApp/Db/pmacStatus.db
+	$IOC_PATH/pmacUtilApp/Db/pmac_asyn_motor.template
