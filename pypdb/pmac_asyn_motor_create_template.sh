54e704807c76be89a3b50245460d6410a071f056
Wayne Lewis <wlewis@bnl.gov>
2012-04-18 21:50:44 -0400

    [#227] Create template and translation script to handle pmac_asyn_motor database.

--- pypdb/pmac_asyn_motor_create_template.sh
+++ pypdb/pmac_asyn_motor_create_template.sh
@@ -2,5 +2,4 @@
 getpvs -I /usr/lib/epics/dbd -I . \
 	-o pmacUtil_pmac_asyn_motor.pot \
 	base.dbd \
-	busySupport.dbd \
 	~/local/data/work/hg/repos/modules/pmacUtil/pmacUtilApp/Db/pmac_asyn_motor.template
