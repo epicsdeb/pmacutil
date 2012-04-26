fadc30f88892cb04ba0e72916b085cb3973439c9
Wayne Lewis <wlewis@bnl.gov>
2012-04-26 13:46:30 -0400

    [#227] Add blank version of motorstatus translation template.

--- pypdb/motorstatus_create_template.sh
+++ pypdb/motorstatus_create_template.sh
@@ -2,4 +2,6 @@
 getpvs -I /usr/lib/epics/dbd -I . \
 	-o pmacUtil_motorstatus.pot \
 	base.dbd \
+	asynRecord.dbd \
+	sCalcoutRecord.dbd \
 	~/local/data/work/hg/repos/modules/pmacUtil/pmacUtilApp/Db/motorstatus.db
