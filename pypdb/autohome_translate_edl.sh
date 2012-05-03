8311542fb9d3d2cb331d3f2529a6fa36d4fe6f5b
Wayne Lewis <wlewis@bnl.gov>
2012-05-03 11:00:30 -0400

    [#227] Add translation scripts for autohome database.

---
+++ pypdb/autohome_translate_edl.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+IOC_PATH=~/local/data/work/hg/repos/modules/pmacUtil
+applypvs  -i $IOC_PATH/pypdb/pmacUtil_autohome.pot \
+	-o $IOC_PATH/fls \
+	$IOC_PATH/pmacUtilApp/opi/edl/autohome.edl
